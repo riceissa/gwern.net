dff694867e11f096aff25641e062e4a6b7538a75
gwern <gwern@gwern.net>
2015-02-24 13:02:14 -0500

    markdown-lint: check for incorrect internal links

--- markdown-lint.sh
+++ markdown-lint.sh
@@ -18,8 +18,8 @@ do
             -e "www.tandfonline.com/doi/abs/" -e "jstor.org" -e "springer.com" -e "springerlink.com" \
             -e "www.mendeley.com" -- "$PAGE";
         egp -e "http://www.pnas.org/content/.*/.*/.*.abstract" -e '[^\.]t\.test\(' -- "$PAGE";
-        fgp -e "<q>" -e "</q>" -e "(www" -e ")www" -e "![](" -e "]()" -e " percent " -e "    Pearson'" \
-            -e '~~~{.sh}' -e 'library("' -- "$PAGE";
+        fgp -e "<q>" -e "</q>" -e "(www" -e ")www" -e "![](" -e "]()" -e "](/wiki/" -e "](wiki/" \
+            -e " percent " -e "    Pearson'" -e '~~~{.sh}' -e 'library("' -- "$PAGE";
 
         # look for personal uses of illegitimate statistics & weasel words, but filter out blockquotes
         fgp -e ' significant ' -e ' significantly ' -e ' obvious' -e 'basically' -- "$PAGE" | egrep -v '[[:space:]]*>';
